632a21a732c354fa42215a3236b666343f9c5019
Jimanity <531757933@qq.com>
2019-06-11 10:14:33 +0800

    SERVLET

--- src/service/registservice/IExistRegistService.java
+++ src/service/registservice/IExistRegistService.java
@@ -11,19 +11,19 @@ import vo.Register;
  * @return
 **/
 public interface IExistRegistService {
-    /**
-     * @Author lym
-     * @Description通过病历号查询一个挂号信息
-     * @Param [caseNumber]
-     * @return vo.Register
-    **/
- Register findRegister(String caseNumber);
- /**
-  * @Author lym
-  * @Description:病历号为caseNumber的患者退号
-  * @Param [caseNumber]
-  * @return boolean
- **/
- boolean existRegist(String caseNumber, Invoice iv, PatientCosts pc);
+        /**
+         * @Author lym
+         * @Description通过病历号查询一个挂号信息
+         * @Param [caseNumber]
+         * @return vo.Register
+        **/
+    Register findRegister(String caseNumber);
+     /**
+      * @Author lym
+      * @Description:病历号为caseNumber的患者退号
+      * @Param [caseNumber]
+      * @return boolean
+     **/
+    boolean existRegist(String caseNumber, Invoice iv, PatientCosts pc);
 
  }
